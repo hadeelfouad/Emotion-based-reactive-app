2355256ecce3d248adb10827319ac5105b896580
Omar Ashraf <omar.mohamed1@emc.com>
2018-03-17 15:43:52 +0200

    first commit

---
+++ assets/slogans.js
@@ -0,0 +1,34 @@
+const slogans =  [
+    {
+        "happy": [
+            "Keep smiling!",
+            "That's another smile!",
+            "You'll like that",
+            "Another great smile",
+            "Have an awesome day",
+            "Keep being joyful"
+        ]
+    },
+    {
+        "funny": [
+            "Why so serious?",
+            "Smile, smile, smile!",
+            "Be happy, always"
+        ]
+    },
+    {
+        "disgust": []
+    },
+    {
+        "contempt": []
+    },
+    {
+        "anger": []
+    },
+    {
+        "fear": []
+    },
+    {
+        "surprise": []
+    }
+]
---
+++ js/app.js
@@ -0,0 +1,44 @@
+let slogansData = slogans;
+let slogansIndex = 0;
+
+renderGIF = function(searchKey) {
+    
+    let gifs;
+    let xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + searchKey + "&api_key=tAhiCMj64bRyq1EzaXsy0eyj6Om2QidE&limit=35");
+    
+    switch(searchKey) {
+        case "happy":
+            slogansIndex = 0; break;
+        case "funny":
+            slogansIndex = 1; break;
+        case "disgust":
+            slogansIndex = 2; break;
+        case "contempt":
+            slogansIndex = 3; break;
+        case "anger":
+            slogansIndex = 4; break;
+        case "fear":
+            slogansIndex = 5; break;
+        case "surprise":
+            slogansIndex = 6; break;
+    }
+
+    xhr.done(function(data) {
+        gifs = data;
+        index = Math.floor((Math.random() * 35));
+
+        let img = document.createElement("img");
+        img.src = gifs.data[index].images.original.url;
+        img.width = 640;
+        img.height = 480;
+        let mainDiv = document.getElementById("gifs");
+        emptyGIF();
+        mainDiv.appendChild(img);
+        
+        
+    });
+}
+
+emptyGIF = function() {
+    document.getElementById("gifs").innerHTML = "";
+}
